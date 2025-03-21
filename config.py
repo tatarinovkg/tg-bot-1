410a4bbcb4fc12de53b0334d3fb3f4cf3076649c
tatarinovkg <155839180+tatarinovkg@users.noreply.github.com>
2025-03-21 23:08:32 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ BOT_TOKEN = 'YOUR-BOT-TOKEN' # @BotFather в телеграме (str)
 bot = Bot(BOT_TOKEN)
 DB_PATH = "YOUR:/DB/PATH" (str)
 
-ADMIN_IDS = ['ADMIN-ID'] # @username_to_id_bot в телеграме (str)
-GROUP_ID = 'GROUP-ID' # @username_to_id_bot в телеграме (int, минус в начале id обязателен)
+ADMIN_IDS = ['ADMIN-ID', 'ADMIN-ID-2'] # @username_to_id_bot в телеграме (str)
+GROUP_ID = -123456789 # @username_to_id_bot в телеграме (int)
