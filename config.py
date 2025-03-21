0acf33590e087ef0ae4daf2c0b69af687bbdae7d
tatarinovkg <155839180+tatarinovkg@users.noreply.github.com>
2025-03-21 22:59:55 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from aiogram import Bot
+
+BOT_TOKEN = 'YOUR-BOT-TOKEN' # @BotFather в телеграме (str)
+bot = Bot(BOT_TOKEN)
+DB_PATH = "YOUR:/DB/PATH" (str)
+
+ADMIN_IDS = ['ADMIN-ID'] # @username_to_id_bot в телеграме (str)
+GROUP_ID = 'GROUP-ID' # @username_to_id_bot в телеграме (int, минус в начале id обязателен)
